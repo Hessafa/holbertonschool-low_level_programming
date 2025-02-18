4fb544f75bf2ae9b0bf6177b0b99c734cf670e66
hessafa <10685@holbertonschool.com>
2025-02-18 16:38:01 +0000

    test

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * _isalpha - checks if a character is an alphabetic letter.
- * @c: the character to be checked
+ * _isalpha - checks if a character is alphabetic (a-z or A-Z).
+ * @c: the character to check
  *
- * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ * Return: 1 if c is an alphabetic character, 0 otherwise.
  */
 int _isalpha(int c)
 {
@@ -16,25 +16,4 @@ int _isalpha(int c)
 	{
 		return (0);
 	}
-}
-
-/**
- * main - Entry point to test the _isalpha function.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char test_char = 'A';
-
-	if (_isalpha(test_char))
-	{
-		printf("%c is an alphabetic character.\n", test_char);
-	}
-	else
-	{
-		printf("%c is not an alphabetic character.\n", test_char);
-	}
-
-	return (0);
 }
