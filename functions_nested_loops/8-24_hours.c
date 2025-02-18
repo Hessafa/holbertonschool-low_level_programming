c2a9966a4bb081dc015ccc5da58b1157e6a9e797
hessafa <10685@holbertonschool.com>
2025-02-18 22:06:28 +0000

    hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer,
- * starting from 00:00 to 23:59
+ * jack_bauer - Prints every minute of the day from 00:00 to 23:59
+ *
  * Return: Nothing
  */
 void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; hour < 24; hour++)  /* Loop through hours (00 to 23) */
+	for (hour = 0; hour < 24; hour++) /* Loop through hours */
 	{
-		for (minute = 0; minute < 60; minute++)  /* Loop through minutes (00 to 59) */
+		for (minute = 0; minute < 60; minute++) /* Loop through minutes */
 		{
-			_putchar('0' + hour / 10);  /* Print the first digit of the hour */
-			_putchar('0' + hour % 10);  /* Print the second digit of the hour */
-			_putchar(':');  /* Print the colon separator */
-			_putchar('0' + minute / 10);  /* Print the first digit of the minute */
-			_putchar('0' + minute % 10);  /* Print the second digit of the minute */
-			_putchar('\n');  /* Print a newline */
+			_putchar((hour / 10) + '0'); /* Print first digit of hour */
+			_putchar((hour % 10) + '0'); /* Print second digit of hour */
+			_putchar(':'); /* Print colon separator */
+			_putchar((minute / 10) + '0'); /* Print first digit of minute */
+			_putchar((minute % 10) + '0'); /* Print second digit of minute */
+			_putchar('\n'); /* Print newline */
 		}
 	}
 }
