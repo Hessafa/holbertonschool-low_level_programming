8f96e597bc6c1f5145d6470b213827fc1b0cb085
Hessafa <10685@holbertonstudents.com>
2025-02-23 21:11:32 +0000

    exclude

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,12 @@
  */
 void print_most_numbers(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i <= 9; i++) /* Loop from 0 to 9 */
-    {
-        if (i != 2 && i != 4) /* Skip 2 and 4 */
-        {
-            _putchar(i + '0'); /* Print the current number */
-        }
-    }
-    _putchar('\n'); /* Print a new line */
+	for (i = 0; i <= 9; i++) /* Loop from 0 to 9 */
+	{
+		if (i != 2 && i != 4) /* Skip 2 and 4 */
+			_putchar(i + '0'); /* Print the current number */
+	}
+	_putchar('\n'); /* Print a new line */
 }
