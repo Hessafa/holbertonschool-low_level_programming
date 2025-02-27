ed29d0316019f0770d7395f5b352255e76756b12
Hessafa <10685@holbertonstudents.com>
2025-02-27 06:30:43 +0000

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+    char *ptr = dest;
+
+    // Move the pointer to the end of the dest string
+    while (*ptr != '\0')
+    {
+        ptr++;
+    }
+
+    // Append the src string to the dest string
+    while (*src != '\0')
+    {
+        *ptr = *src;
+        ptr++;
+        src++;
+    }
+
+    // Add the terminating null byte
+    *ptr = '\0';
+
+    return dest;
+}
