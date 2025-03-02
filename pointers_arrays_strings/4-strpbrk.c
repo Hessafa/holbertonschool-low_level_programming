a4378aaafdaad83998f7d733491ad894e3d8c8a7
root <10685@holbertonstudents.com>
2025-03-02 10:40:59 +0000

    pbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -5,8 +5,6 @@
  * @s: Pointer to the string to search.
  * @accept: Pointer to the string containing bytes to search for.
  *
- * Return: A pointer to the byte in `s` that matches one of the bytes in `accept`,
- *         or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
