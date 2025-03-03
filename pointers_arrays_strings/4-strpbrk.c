6e198d0e992dfb16c252bdbd098da2e534354cb0
Hessafa <10685@holbertonstudents.com>
2025-03-03 08:30:01 +0000

    strpk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -5,6 +5,8 @@
  * @s: Pointer to the string to search.
  * @accept: Pointer to the string containing bytes to search for.
  *
+ * Return: Pointer to the byte in s that matches one of the bytes in accept,
+ *         or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
