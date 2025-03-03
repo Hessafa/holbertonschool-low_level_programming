e204eb723da3b0864359395ba2c7245044257192
Hessafa <10685@holbertonstudents.com>
2025-03-03 09:00:32 +0000

    diagsum

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -33,3 +33,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
