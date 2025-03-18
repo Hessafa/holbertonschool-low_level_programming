ccf0fdb109a42da05a1fa05a9ee564fe6eaa0de9
Hessafa <10685@holbertonstudents.com>
2025-03-18 07:44:09 +0000

    s

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,53 +1,34 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
-/**
- * print_name - prints a name using the function pointed to by f
- * @name: name of the person
- * @f: function pointer to a function that prints a name
- *
- * Return: nothing
- */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name && f) /* Check if both name and function pointer are valid */
-		f(name); /* Call the function pointed to by f */
+    if (name && f) /* Check if both name and function pointer are valid */
+        f(name); /* Call the function pointed to by f */
 }
 
-/**
- * print_name_as_is - prints a name as is
- * @name: name of the person
- *
- * Return: nothing
- */
 void print_name_as_is(char *name)
 {
-	printf("Hello, my name is %s\n", name);
+    printf("Hello, my name is %s\n", name);
 }
 
-/**
- * print_name_uppercase - prints a name in uppercase
- * @name: name of the person
- *
- * Return: nothing
- */
 void print_name_uppercase(char *name)
 {
-	unsigned int i;
+    unsigned int i;
 
-	printf("Hello, my uppercase name is ");
-	i = 0;
-	while (name[i]) /* Loop through each character of the name */
-	{
-		if (name[i] >= 'a' && name[i] <= 'z')
-		{
-			putchar(name[i] - 'a' + 'A'); /* Convert to uppercase */
-		}
-		else
-		{
-			putchar(name[i]); /* Print as is */
-		}
-		i++;
-	}
-	putchar('\n'); /* Print newline after name */
+    printf("Hello, my uppercase name is ");
+    i = 0;
+    while (name[i]) /* Loop through each character of the name */
+    {
+        if (name[i] >= 'a' && name[i] <= 'z')
+        {
+            putchar(name[i] - 'a' + 'A'); /* Convert to uppercase */
+        }
+        else
+        {
+            putchar(name[i]); /* Print as is */
+        }
+        i++;
+    }
+    putchar('\n'); /* Print newline after name */
 }
