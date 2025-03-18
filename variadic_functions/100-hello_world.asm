3694336840b279b72a1b5155f325324d58a40afa
Hessafa <10685@holbertonstudents.com>
2025-03-18 10:59:27 +0000

    hello

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,20 +1,18 @@
 section .data
-    ; Define the string to be printed
-    hello db 'Hello, World', 0xA  ; 'Hello, World' followed by a newline (0xA)
-    hello_len equ $ - hello        ; Calculate the length of the string
+    msg db 'Hello, World', 10  ; "Hello, World" string with newline (10)
 
 section .text
-    global _start                  ; Entry point for the program
+    global _start
 
 _start:
-    ; System call: write(1, hello, hello_len)
-    mov rax, 1                     ; syscall number for write (1)
-    mov rdi, 1                     ; file descriptor (1 = stdout)
-    mov rsi, hello                 ; pointer to the string to print
-    mov rdx, hello_len             ; length of the string
-    syscall                        ; invoke the system call
+    ; write system call (syscall number 1)
+    mov rax, 1            ; syscall number for write
+    mov rdi, 1            ; file descriptor 1 (stdout)
+    mov rsi, msg          ; pointer to the message
+    mov rdx, 14           ; length of the message (13 characters + newline)
+    syscall               ; make the system call
 
-    ; System call: exit(0)
-    mov rax, 60                    ; syscall number for exit (60)
-    xor rdi, rdi                   ; exit code (0)
-    syscall                        ; invoke the system call
+    ; exit system call (syscall number 60)
+    mov rax, 60           ; syscall number for exit
+    xor rdi, rdi          ; exit code 0
+    syscall               ; make the system call
