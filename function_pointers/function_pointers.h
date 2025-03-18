9ff9f5237f26395a50c69674f190eac95faf325e
Hessafa <10685@holbertonstudents.com>
2025-03-18 07:49:30 +0000

    h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -6,4 +6,4 @@ void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
 
-#endif /* FUNCTION_POINTERS_H */:
+#endif /* FUNCTION_POINTERS_H */
