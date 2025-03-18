011ba266fc9b08e060d898520b2bc6841b9c4cf8
Hessafa <10685@holbertonstudents.com>
2025-03-18 09:18:46 +0000

    s

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,34 +1,12 @@
 #include "function_pointers.h"
-#include <stdio.h>
+
+/**
+ * print_name - Print a name from a function pointer
+ * @name: char string
+ * @f: function pointer that takes a string argument
+ */
 
 void print_name(char *name, void (*f)(char *))
 {
-    if (name && f) /* Check if both name and function pointer are valid */
-        f(name); /* Call the function pointed to by f */
-}
-
-void print_name_as_is(char *name)
-{
-    printf("Hello, my name is %s\n", name);
-}
-
-void print_name_uppercase(char *name)
-{
-    unsigned int i;
-
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i]) /* Loop through each character of the name */
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] - 'a' + 'A'); /* Convert to uppercase */
-        }
-        else
-        {
-            putchar(name[i]); /* Print as is */
-        }
-        i++;
-    }
-    putchar('\n'); /* Print newline after name */
+	f(name);
 }
