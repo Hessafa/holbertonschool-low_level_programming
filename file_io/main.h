103b922be6fe0b7831bcfb71e1e86487bde1a69e
Hessafa <10685@holbertonstudents.com>
2025-04-02 01:27:27 +0000

    s

--- file_io/main.h
+++ file_io/main.h
@@ -11,4 +11,6 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 void copy_content(int argc, char *argv[]);
+void copy_content(char *file_from, char *file_to);
+int _putchar(char c);
 #endif /* MAIN_H */
