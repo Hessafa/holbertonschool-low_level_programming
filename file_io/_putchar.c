8fdb8cafcf8b1db1cd808142ecf16e56f05b848e
Hessafa <10685@holbertonstudents.com>
2025-04-02 01:34:36 +0000

    s

---
+++ file_io/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: The character to print
+ * Return: On success 1, -1 on error
+ */
+int _putchar(char c)
+{
+return (write(0, &c, 1));
+}
