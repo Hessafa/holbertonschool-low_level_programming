4acf39edd1c7f992056cf2907762c982ce2ca614
Hessafa <10685@holbertonstudents.com>
2025-04-05 00:40:00 +0000

    1st task

--- file_io/main.h
+++ file_io/main.h
@@ -1,17 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdlib.h>
-#include <stddef.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+
 #include <unistd.h>
-void display_elf_header(Elf64_Ehdr *header);
-void print_error(char *message);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <fcntl.h>
+
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void copy_content(int argc, char *argv[]);
-void copy_content(char *file_from, char *file_to);
-int _putchar(char c);
 #endif /* MAIN_H */
